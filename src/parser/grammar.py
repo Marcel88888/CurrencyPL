5342961ecbd1cc3c922ccdbe5ada616b2cf649a6
Marcel88888 <marcel.kawski1@gmail.com>
2020-12-29 15:09:40 +0100

    parsing blocks and statements implemented

--- src/parser/grammar.py
+++ src/parser/grammar.py
@@ -9,3 +9,13 @@ class Signature:
     def __init__(self, _type, _id):
         self.type = _type
         self.id = _id
+
+
+class Parameters:
+    def __init__(self, signatures):
+        self.signatures = signatures
+
+
+class Block:
+    def __init__(self, statements):
+        self.statements = statements
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -24,7 +24,6 @@ class Parser:
                 self.__lexer.get_next_token()
                 parameters = self.parse_parameters()
                 if parameters is not None:
-                    self.__lexer.get_next_token()
                     if self.__lexer.token.type == TokenTypes.CL_BRACKET:
                         self.__lexer.get_next_token()
                         if self.__lexer.token.type == TokenTypes.OP_CURLY_BRACKET:
@@ -34,10 +33,10 @@ class Parser:
                                 self.__lexer.get_next_token()
                                 if self.__lexer.token.type == TokenTypes.CL_CURLY_BRACKET:
                                     return FunctionDef(signature, parameters, block)
-                return _SyntaxError(self.__lexer.line, self.__lexer.column)
+            return _SyntaxError(self.__lexer.line, self.__lexer.column)
         return None
 
-    def parse_signature(self):
+    def parse_signature(self):  # type, id ;
         token_type = self.__lexer.token.type
         if token_type in self.__function_data_types:
             _type = token_type
@@ -45,11 +44,71 @@ class Parser:
             if self.__lexer.token.type == TokenTypes.IDENTIFIER:
                 _id = self.__lexer.token.value
                 return Signature(_type, _id)
+            raise _SyntaxError(self.__lexer.line, self.__lexer.column)
         return None
 
     def parse_parameters(self):  # [ signature, { “,”, signature } ];
-        self.__lexer.get_next_token()
-
-
-    def parse_block(self):
+        signatures = []
+        signature = self.parse_signature()
+        if signature is not None:
+            signatures.append(signature)
+            self.__lexer.get_next_token()
+            while self.__lexer.token.type == TokenTypes.COMMA:
+                self.__lexer.get_next_token()
+                signature = self.parse_signature()
+                if signature is not None:
+                    signatures.append(signature)
+                else:
+                    raise _SyntaxError(self.__lexer.line, self.__lexer.column)
+                self.__lexer.get_next_token()
+            return Parameters(signatures)
         return None
+
+    def parse_block(self):  # { statement };
+        statements = []
+        statement = self.parse_statement()
+        while statement is not None:
+            statements.append(statement)
+            self.__lexer.get_next_token()
+            statement = self.parse_statement()
+        return Block(statements)
+
+    def parse_statement(self):  # ifStatement | whileStatement | returnStatement | initStatement | assignStatement |
+        # functionCall ;
+        statement = self.parse_if_statement()
+        if statement is not None:
+            return statement
+        statement = self.parse_while_statement()
+        if statement is not None:
+            return statement
+        statement = self.parse_return_statement()
+        if statement is not None:
+            return statement
+        statement = self.parse_init_statement()
+        if statement is not None:
+            return statement
+        statement = self.parse_assign_statement()
+        if statement is not None:
+            return statement
+        statement = self.parse_function_call()
+        if statement is not None:
+            return statement
+        return None
+
+    def parse_if_statement(self):
+        pass
+
+    def parse_while_statement(self):
+        pass
+
+    def parse_return_statement(self):
+        pass
+
+    def parse_init_statement(self):
+        pass
+
+    def parse_assign_statement(self):
+        pass
+
+    def parse_function_call(self):
+        pass
