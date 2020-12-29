b660a9d0a8f541b26705a1fb6300225a692f7e1c
Marcel88888 <marcel.kawski1@gmail.com>
2020-12-29 14:33:47 +0100

    parse_function_def() corrected, parse_signature() implemented and Signature class added

--- src/parser/grammar.py
+++ src/parser/grammar.py
@@ -1,6 +1,11 @@
 class FunctionDef:  # signature, “(”, parameters, “)”, “{“, block, “}” ;
-    def __init__(self, _type, _id, parameters, block):
-        self.type = _type
-        self.id = _id
+    def __init__(self, signature, parameters, block):
+        self.signature = signature
         self.parameters = parameters
         self.block = block
+
+
+class Signature:
+    def __init__(self, _type, _id):
+        self.type = _type
+        self.id = _id
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -6,6 +6,7 @@ from .grammar import *
 class Parser:
     def __init__(self, lexer):
         self.__lexer = lexer
+        self.__function_data_types = [TokenTypes.DECIMAL, TokenTypes.CURRENCY, TokenTypes.VOID]
 
     def parse_program(self):
         function_defs = []
@@ -15,46 +16,40 @@ class Parser:
             function_def = self.parse_function_def()
 
     def parse_function_def(self):  # signature, “(”, parameters, “)”, “{“, block, “}” ;
+        self.__lexer.get_next_token()
+        signature = self.parse_signature()
+        if signature is not None:
+            self.__lexer.get_next_token()
+            if self.__lexer.token.type == TokenTypes.OP_BRACKET:
+                self.__lexer.get_next_token()
+                parameters = self.parse_parameters()
+                if parameters is not None:
+                    self.__lexer.get_next_token()
+                    if self.__lexer.token.type == TokenTypes.CL_BRACKET:
+                        self.__lexer.get_next_token()
+                        if self.__lexer.token.type == TokenTypes.OP_CURLY_BRACKET:
+                            self.__lexer.get_next_token()
+                            block = self.parse_block()
+                            if block is not None:
+                                self.__lexer.get_next_token()
+                                if self.__lexer.token.type == TokenTypes.CL_CURLY_BRACKET:
+                                    return FunctionDef(signature, parameters, block)
+                return _SyntaxError(self.__lexer.line, self.__lexer.column)
+        return None
+
+    def parse_signature(self):
         token_type = self.__lexer.token.type
-        if token_type == TokenTypes.DECIMAL or token_type == TokenTypes.CURRENCY or token_type == TokenTypes.VOID:
+        if token_type in self.__function_data_types:
             _type = token_type
             self.__lexer.get_next_token()
             if self.__lexer.token.type == TokenTypes.IDENTIFIER:
                 _id = self.__lexer.token.value
-                self.__lexer.get_next_token()
-                if self.__lexer.token.type == TokenTypes.OP_BRACKET:
-                    self.__lexer.get_next_token()
-                    parameters = self.parse_parameters()
-                    if parameters is not None:
-                        self.__lexer.get_next_token()
-                        if self.__lexer.token.type == TokenTypes.CL_BRACKET:
-                            self.__lexer.get_next_token()
-                            if self.__lexer.token.type == TokenTypes.OP_CURLY_BRACKET:
-                                self.__lexer.get_next_token()
-                                block = self.parse_block()
-                                if block is not None:
-                                    self.__lexer.get_next_token()
-                                    if self.__lexer.token.type == TokenTypes.CL_CURLY_BRACKET:
-                                        return FunctionDef(_type, _id, parameters, block)
-                                    else:
-                                        raise _SyntaxError(self.__lexer.line, self.__lexer.column)
-                                else:
-                                    raise _SyntaxError(self.__lexer.line, self.__lexer.column)
-                            else:
-                                raise _SyntaxError(self.__lexer.line, self.__lexer.column)
-                        else:
-                            raise _SyntaxError(self.__lexer.line, self.__lexer.column)
-                    else:
-                        raise _SyntaxError(self.__lexer.line, self.__lexer.column)
-                else:
-                    return None
-            else:
-                return None
-        else:
-            return None
-
-    def parse_parameters(self):
+                return Signature(_type, _id)
         return None
+
+    def parse_parameters(self):  # [ signature, { “,”, signature } ];
+        self.__lexer.get_next_token()
+
 
     def parse_block(self):
         return None
